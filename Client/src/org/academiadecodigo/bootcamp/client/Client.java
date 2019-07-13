d89c744c013372a73045ce220a8ba07defec9396
Bruno Rebelo <rebelob@hotmail.com>
2019-07-13 14:41:39 +0000

    First Client commit

---
+++ Client/src/org/academiadecodigo/bootcamp/client/Client.java
@@ -0,0 +1,110 @@
+package org.academiadecodigo.bootcamp.client;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+        private Socket activeSocket;
+        private BufferedWriter out;
+        private BufferedReader in;
+        private BufferedReader inputMsg;
+
+
+        Client() {
+            try {
+                inputMsg = new BufferedReader(new InputStreamReader(System.in));
+                activeSocket = new Socket(host(), port());
+                out = new BufferedWriter(new OutputStreamWriter(activeSocket.getOutputStream()));
+                in = new BufferedReader(new InputStreamReader(activeSocket.getInputStream()));
+                nick();
+            } catch (IOException ioEx) {
+                System.out.println(ioEx.getMessage());
+            }
+        }
+
+
+        private String host(){
+            try {
+                System.out.println("\nEnter Host: ");
+                String host = null;
+
+                host = inputMsg.readLine();
+
+                return host;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private int port(){
+            try {
+                System.out.println("\nEnter Port: ");
+                int port = 0;
+                port = Integer.parseInt(inputMsg.readLine());
+                return port;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return Integer.parseInt(null);
+        }
+
+        private void nick(){
+            try {
+                char[] sentBuffer;
+                System.out.println("\nEnter Nickname: ");
+                sentBuffer = ("\\nick " + inputMsg.readLine()).toCharArray();
+
+                out.write(sentBuffer, 0, sentBuffer.length);
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        void init(){
+            System.out.println("Connection established.");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (!activeSocket.isClosed()){
+                        receive();
+                    }
+                }
+            }).start();
+            while (!this.activeSocket.isClosed()) {
+                this.send();
+            }
+        }
+
+        private void send() {
+            try {
+                char[] sentBuffer;
+                String inputMessage = inputMsg.readLine();
+
+                sentBuffer = inputMessage.toCharArray();
+
+                out.write(sentBuffer, 0, sentBuffer.length);
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void receive() {
+            try {
+                char[] receiveBuffer = new char[1028];
+                int readCount = 0;
+                readCount = in.read(receiveBuffer);
+                String receiveBufferStr = String.valueOf(receiveBuffer, 0, readCount);
+                if(receiveBufferStr.substring(0, 1).equals("\\")) {
+                    //game logic
+                } else {
+                    System.out.println(receiveBufferStr);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
---
+++ Client/src/org/academiadecodigo/bootcamp/client/Main.java
@@ -0,0 +1,10 @@
+package org.academiadecodigo.bootcamp.client;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.init();
+    }
+}
